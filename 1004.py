df1b7eddf0d7a083810c97986036bcf263e3af9e
André <110499972+4ndreLeal@users.noreply.github.com>
2022-09-24 16:39:44 -0300

    Simple Product

--- 1004.py
+++ 1004.py
@@ -1,5 +1,6 @@
 '''
-Para a resolução do problema '1004 - Simple Product' vale a mesma explicação dada no problema '1003 - Simple Sum'
+Para a resolução do problema '1004 - Simple Product' vale a mesma explicação dada no problema '1003 - Simple Sum'.
+O produto de dois números inteiros armazenados nas variáveis A e B é A * B, que também pode ser armazenado na variável PROD.
 '''
 
 A = int(input())
