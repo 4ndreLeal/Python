93b94876ba05db75997ab60efd6cf7bbcdec9b20
André <110499972+4ndreLeal@users.noreply.github.com>
2022-09-24 16:36:14 -0300

    Simple Product

---
+++ 1004.py
@@ -0,0 +1,8 @@
+'''
+Para a resolução do problema '1004 - Simple Product' vale a mesma explicação dada no problema '1003 - Simple Sum'
+'''
+
+A = int(input())
+B = int(input())
+PROD = A * B
+print('PROD = {}'.format(PROD))
